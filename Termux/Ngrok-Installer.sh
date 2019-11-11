43311fac44ea047e3ce5dad0dbf9c3e8917abb20
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2019-11-11 11:39:37 +0700

    Update Ngrok Installer

--- Termux/Ngrok-Installer.sh
+++ Termux/Ngrok-Installer.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-apt update && apt upgrade -y
+apt update && apt full-upgrade -y
 apt install wget unzip -y
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip -O ~/ngrok.zip
 unzip ngrok.zip -d $PREFIX/bin
