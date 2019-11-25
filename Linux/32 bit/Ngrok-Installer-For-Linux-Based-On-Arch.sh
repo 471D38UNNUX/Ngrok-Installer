f3218776013b72a555e3bf0c67b17a146df10fc0
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2019-11-25 15:46:48 +0700

    Update Ngrok Installer

---
+++ Linux/32 bit/Ngrok-Installer-For-Linux-Based-On-Arch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo sudo pacman -Syu
+sudo pacman -S wget zip
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip -O ~/ngrok.zip
+sudo unzip ngrok.zip -d /usr/bin 
+rm -rf ~/ngrok.zip
+
+ngrok
