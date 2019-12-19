f5a440614ef57519bf7ebb4d40e868d159625cad
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2019-12-19 13:18:32 +0700

    Update Ngrok Installer

--- Windows/64 bit/Ngrok-Installer.bat
+++ Windows/64 bit/Ngrok-Installer.bat
@@ -4,5 +4,6 @@ setx /M PATH "%PATH%;C:\Program Files\WinRAR;C:\ngrok;"
 curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -o C:\Users\%username%\Downloads\ngrok-stable-windows-amd64.zip
 winrar x C:\Users\user\Downloads\ngrok-stable-windows-amd64.zip ngrok.exe C:\ngrok\
 del C:\Users\%username%\Downloads\ngrok-stable-windows-amd64.zip
+del C:\Users\%username%\Downloads\Wincurl
 
 ngrok
