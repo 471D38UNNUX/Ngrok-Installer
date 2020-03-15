b4eb49d3ee31492aeaae4424791af4d33623fd87
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2020-03-15 09:40:16 +0700

    Update Ngrok Installer

--- Linux/64 bit/Ngrok-Installer-For-Linux-Based-On-SUSE.sh
+++ Linux/64 bit/Ngrok-Installer-For-Linux-Based-On-SUSE.sh
@@ -3,7 +3,7 @@
 sudo zypper dup -y
 sudo zypper install wget zip -y
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ~/ngrok.zip
-sudo unzip ngrok.zip -d /usr/bin 
+sudo unzip ~/ngrok.zip -d /usr/bin 
 rm -rf ~/ngrok.zip
 
 ngrok
