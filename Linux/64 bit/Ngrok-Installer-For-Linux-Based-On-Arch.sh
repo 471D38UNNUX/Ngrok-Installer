6a6ba1e012e1d82da60ae90ebb560bf8fa09d18e
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2020-03-15 09:28:57 +0700

    Update Ngrok Installer

--- Linux/64 bit/Ngrok-Installer-For-Linux-Based-On-Arch.sh
+++ Linux/64 bit/Ngrok-Installer-For-Linux-Based-On-Arch.sh
@@ -3,7 +3,7 @@
 sudo sudo pacman -Syu
 sudo pacman -S wget zip
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ~/ngrok.zip
-sudo unzip ngrok.zip -d /usr/bin 
+sudo unzip ~/ngrok.zip -d /usr/bin 
 rm -rf ~/ngrok.zip
 
 ngrok
