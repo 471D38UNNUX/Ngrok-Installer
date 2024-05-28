ee1dc31192f73a6bf521988dd94fe23509b84bfb
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2024-05-29 01:55:32 +0700

    Delete Linux/64 bit directory

--- Linux/64 bit/Ngrok-Installer-For-Linux-Based-On-Debian.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo apt update && sudo apt full-upgrade -y
-sudo apt install wget zip -y
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ~/ngrok.zip
-sudo unzip ~/ngrok.zip -d /usr/bin 
-rm -rf ~/ngrok.zip
-
-ngrok
