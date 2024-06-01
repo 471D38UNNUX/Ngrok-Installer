19bf376e734fb976a9ad5ef8297ccbf5a4dae4cc
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2024-06-02 00:55:00 +0700

    Delete Windows/64 bit directory

--- Windows/64 bit/Ngrok-Installer.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-setx /M PATH "%PATH%;C:\Program Files\WinRAR;C:\ngrok;"
-curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -o C:\Users\%username%\Downloads\ngrok-stable-windows-amd64.zip
-winrar x C:\Users\user\Downloads\ngrok-stable-windows-amd64.zip ngrok.exe C:\ngrok\
-del C:\Users\%username%\Downloads\ngrok-stable-windows-amd64.zip
-
-ngrok
